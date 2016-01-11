222ad10651a94bdd438abc0f2a88484ea94d0d30
Benjamin Kobjolke <spamprotect@xida.de>
2016-01-11 19:22:05 +0100

    FEATURE (android)
    basic downloads:// url prefix support

--- src/android/cl/kunder/webview/WebViewActivity.java
+++ src/android/cl/kunder/webview/WebViewActivity.java
@@ -3,6 +3,9 @@ package cl.kunder.webview;
 import android.os.Bundle;
 
 import org.apache.cordova.CordovaActivity;
+import android.content.ContextWrapper;
+import java.io.*;
+import android.widget.Toast;
 
 public class WebViewActivity extends CordovaActivity {
     @Override
@@ -10,6 +13,24 @@ public class WebViewActivity extends CordovaActivity {
         super.onCreate(savedInstanceState);
         Bundle b = getIntent().getExtras();
         String url = b.getString("url");
-        loadUrl((url.matches("^(.*://|javascript:)[\\s\\S]*$")?"":"file:///android_asset/www/")+url);
+		if(url.startsWith("file:///")) {
+			loadUrl(url);
+		} else if (url.startsWith("downloads://")) {
+			String[] parts = url.split("downloads://");
+			ContextWrapper contextWrapper = new ContextWrapper(this);
+			
+			String cUrl = "file:///" + contextWrapper.getFilesDir().toString() + "/files/downloads/" + parts[1];
+			
+			/*
+			int duration = Toast.LENGTH_LONG;
+			Toast toast = Toast.makeText(this, cUrl, duration);
+			toast.show();
+			*/
+			
+			loadUrl(cUrl);
+			
+		} else {
+			loadUrl((url.matches("^(.*://|javascript:)[\\s\\S]*$")?"":"file:///android_asset/www/")+url);
+		}
     }
 }
